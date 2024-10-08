007551dc6cb4a5d8cbaae52e91e9c10cabfa8d92
noname <danpod1001@mail.ru>
2024-10-08 21:49:33 +0300

    contracts labels added

--- src/Front/MainWindow.cpp
+++ src/Front/MainWindow.cpp
@@ -187,26 +187,111 @@ MainWindow::MainWindow(QWidget *parent) {
     connect(open_health_contract_button_, &QPushButton::released, this, &MainWindow::openHealthContract);
 
     health_contract_window_ = new QMainWindow();
-    health_contract_window_->resize(400, 550);
+    health_contract_window_->resize(400, 450);
     health_contract_window_->hide();
 
     health_contract_bg_ = new QLabel(health_contract_window_);
     health_contract_bg_->resize(400, 550);
     health_contract_bg_->setPixmap(QPixmap::fromImage(QImage("../src/Front/resources/bg.png")));
 
-    auto health_contract_text_ = new QLabel(health_contract_bg_);
+    health_contract_text_ = new QLabel(health_contract_bg_);
     health_contract_text_->resize(380, 530);
     health_contract_text_->move(10, 10);
     health_contract_text_->setFont(QFont(font_family_, 20));
     health_contract_text_->setStyleSheet("QLabel {"
                                          "color: rgb(0, 0, 0);"
                                          "}");
-    health_contract_text_->setText("By this agreement you confirm that you are enslaving yourself to our company."
-                                   "In turn, in case of health problems,\n we undertake to provide\n you with money\n in the amount of up to ...\n"
-                                   "This agreement is valid\n for ... months from the date of signing.\n"
-                                   "Cost of conclusion: ...");
+    health_contract_text_->setText("HEALTH AGREEMENT\n\nBy this agreement you confirm that you are enslaving yourself to our company."
+                                   " In turn, in case of health problems, we undertake to provide you with money in the amount "
+                                   "of up to " + QString::fromStdString(std::to_string(health_contract_.max_pay)) + "₽\n"
+                                   "This agreement is valid for " + QString::fromStdString(std::to_string(health_contract_.month_count)) + " months from the date of signing."
+                                   " Cost of conclusion: " +
+                                   QString::fromStdString(std::to_string(health_contract_.price))+ "₽\n\n");
     health_contract_text_->setWordWrap(true);
-    health_contract_text_->setAlignment(Qt::AlignLeft);
+    health_contract_text_->setAlignment(Qt::AlignHCenter);
+
+
+    health_label_month_editor_ = new QLabel(health_contract_bg_);
+    health_label_month_editor_->resize(120, 50);
+    health_label_month_editor_->move(15, 290);
+    health_label_month_editor_->setFont(QFont(font_family_, 18));
+    health_label_month_editor_->setAlignment(Qt::AlignLeft);
+    health_label_month_editor_->setStyleSheet("QLabel {"
+                                 "color: rgb(0, 0, 0);"
+                                 "}");
+    health_label_month_editor_->setText("month count");
+
+    health_contract_month_editor_ = new QSpinBox(health_contract_bg_);
+    health_contract_month_editor_->setStyleSheet("QSpinBox {"
+                                       "color: #000000;"
+                                       "background: rgba(120, 120, 120, 40);"
+                                       "border-radius: 7px;"
+                                       "}");
+    health_contract_month_editor_->resize(120, 25);
+    health_contract_month_editor_->move(10, 320);
+    health_contract_month_editor_->setRange(6, 24);
+    health_contract_month_editor_->setValue(health_contract_.month_count);
+    health_contract_month_editor_->setAlignment(Qt::AlignCenter);
+    health_contract_month_editor_->setFont(QFont(font_family_, 15));
+
+    health_label_max_pay_editor_ = new QLabel(health_contract_bg_);
+    health_label_max_pay_editor_->resize(120, 50);
+    health_label_max_pay_editor_->move(140, 290);
+    health_label_max_pay_editor_->setFont(QFont(font_family_, 18));
+    health_label_max_pay_editor_->setAlignment(Qt::AlignHCenter);
+    health_label_max_pay_editor_->setStyleSheet("QLabel {"
+                                              "color: rgb(0, 0, 0);"
+                                              "}");
+    health_label_max_pay_editor_->setText("max pay");
+
+    health_contract_max_pay_editor_ = new QSpinBox(health_contract_bg_);
+    health_contract_max_pay_editor_->setStyleSheet("QSpinBox {"
+                                                 "color: #000000;"
+                                                 "background: rgba(120, 120, 120, 40);"
+                                                 "border-radius: 7px;"
+                                                 "}");
+    health_contract_max_pay_editor_->resize(120, 25);
+    health_contract_max_pay_editor_->move(140, 320);
+    health_contract_max_pay_editor_->setRange(1, 100000);
+    health_contract_max_pay_editor_->setValue(health_contract_.max_pay);
+    health_contract_max_pay_editor_->setAlignment(Qt::AlignCenter);
+    health_contract_max_pay_editor_->setFont(QFont(font_family_, 15));
+
+    health_label_price_editor_ = new QLabel(health_contract_bg_);
+    health_label_price_editor_->resize(120, 50);
+    health_label_price_editor_->move(270, 290);
+    health_label_price_editor_->setFont(QFont(font_family_, 18));
+    health_label_price_editor_->setAlignment(Qt::AlignHCenter);
+    health_label_price_editor_->setStyleSheet("QLabel {"
+                                                "color: rgb(0, 0, 0);"
+                                                "}");
+    health_label_price_editor_->setText("price");
+
+    health_contract_price_editor_ = new QSpinBox(health_contract_bg_);
+    health_contract_price_editor_->setStyleSheet("QSpinBox {"
+                                                   "color: #000000;"
+                                                   "background: rgba(120, 120, 120, 40);"
+                                                   "border-radius: 7px;"
+                                                   "}");
+    health_contract_price_editor_->resize(120, 25);
+    health_contract_price_editor_->move(270, 320);
+    health_contract_price_editor_->setRange(1, 100000);
+    health_contract_price_editor_->setValue(health_contract_.price);
+    health_contract_price_editor_->setAlignment(Qt::AlignCenter);
+    health_contract_price_editor_->setFont(QFont(font_family_, 15));
+
+
+    health_set_month_count_button_ = new QPushButton(health_contract_bg_);
+    health_set_month_count_button_->resize(140, 40);
+    health_set_month_count_button_->move(130, 360);
+    health_set_month_count_button_->setFont(QFont(font_family_, 24));
+    health_set_month_count_button_->setText("set");
+    health_set_month_count_button_->setStyleSheet("QPushButton {"
+                                             "color: #000000;"
+                                             "background: rgba(0, 0, 0, 30);"
+                                             "border-radius: 10px;"
+                                             "}");
+    connect(health_set_month_count_button_, &QPushButton::released, this, &MainWindow::setHealthContract);
 
 
     open_car_contract_button_ = new QPushButton(this);
@@ -229,6 +314,23 @@ MainWindow::MainWindow(QWidget *parent) {
     car_contract_bg_->resize(400, 550);
     car_contract_bg_->setPixmap(QPixmap::fromImage(QImage("../src/Front/resources/bg.png")));
 
+    car_contract_text_ = new QLabel(car_contract_bg_);
+    car_contract_text_->resize(380, 530);
+    car_contract_text_->move(10, 10);
+    car_contract_text_->setFont(QFont(font_family_, 20));
+    car_contract_text_->setStyleSheet("QLabel {"
+                                         "color: rgb(0, 0, 0);"
+                                         "}");
+    car_contract_text_->setText("CAR AGREEMENT\n\nBy this agreement you confirm that you are enslaving yourself to our company."
+                                   " In turn, in case of car breakdown, we undertake to provide you with money in the amount "
+                                   "of up to " + QString::fromStdString(std::to_string(car_contract_.max_pay)) + "₽\n"\
+                                   "This agreement is valid for " + QString::fromStdString(std::to_string(car_contract_.month_count))
+                                   + " months from the date of signing."\
+                                   " Cost of conclusion: " +
+                                   QString::fromStdString(std::to_string(car_contract_.price))+ "₽");
+    car_contract_text_->setWordWrap(true);
+    car_contract_text_->setAlignment(Qt::AlignHCenter);
+
 
     open_house_contract_button_ = new QPushButton(this);
     open_house_contract_button_->resize(150, 30);
@@ -250,6 +352,24 @@ MainWindow::MainWindow(QWidget *parent) {
     house_contract_bg_->resize(400, 550);
     house_contract_bg_->setPixmap(QPixmap::fromImage(QImage("../src/Front/resources/bg.png")));
 
+    house_contract_text_ = new QLabel(house_contract_bg_);
+    house_contract_text_->resize(380, 530);
+    house_contract_text_->move(10, 10);
+    house_contract_text_->setFont(QFont(font_family_, 20));
+    house_contract_text_->setStyleSheet("QLabel {"
+                                      "color: rgb(0, 0, 0);"
+                                      "}");
+    house_contract_text_->setText("HOUSE AGREEMENT\n\nBy this agreement you confirm that you are enslaving yourself to our company."
+                                " In turn, in case of fire in house, we undertake to provide you with money in the amount "
+                                "of up to " + QString::fromStdString(std::to_string(house_contract_.max_pay)) + "₽\n"\
+                                   "This agreement is valid for " + QString::fromStdString(std::to_string(house_contract_.month_count))
+                                + " months from the date of signing."\
+                                   " Cost of conclusion: " +
+                                QString::fromStdString(std::to_string(house_contract_.price))+ "₽");
+    house_contract_text_->setWordWrap(true);
+    house_contract_text_->setAlignment(Qt::AlignHCenter);
+
+
     expense_drawing_ = new QTimer(this);
     connect(expense_drawing_, &QTimer::timeout, this, &MainWindow::drawExpense);
 
@@ -258,6 +378,21 @@ MainWindow::MainWindow(QWidget *parent) {
 
     capital_drawing_ = new QTimer(this);
     connect(capital_drawing_, &QTimer::timeout, this, &MainWindow::drawCapital);
+}
+
+void MainWindow::setHealthContract() {
+    health_contract_.month_count = health_contract_month_editor_->value();
+    health_contract_.max_pay = health_contract_max_pay_editor_->value();
+    health_contract_.price = health_contract_price_editor_->value();
+    health_contract_text_->setText("HEALTH AGREEMENT\n\nBy this agreement you confirm that you are enslaving yourself to our company."
+                                   " In turn, in case of health problems, we undertake to provide you with money in the amount "
+                                   "of up to " + QString::fromStdString(std::to_string(health_contract_.max_pay)) + "₽\n"\
+                                   "This agreement is valid for " +
+                                   QString::fromStdString(std::to_string(health_contract_.month_count)) +
+                                   " months from the date of signing."\
+                                   " Cost of conclusion: " +
+                                   QString::fromStdString(std::to_string(health_contract_.price))+ "₽\n\n");
+    std::cout << "OK\n";
 }
 
 void MainWindow::openHealthContract() {
@@ -321,6 +456,9 @@ void MainWindow::setParams(int month_count, int start_capital, int tax_percentag
     cur_month_->setText(QString::fromStdString(std::to_string(cur_month_id_) +
                                                "/" + std::to_string(month_count_) + " month"));
     company_.set_cur_balance(start_capital);
+    company_.set_car_insurance_demand(base_demand);
+    company_.set_home_insurance_demand(base_demand);
+    company_.set_life_insurance_demand(base_demand);
     tax_percentage_ = tax_percentage;
     base_demand_ = base_demand;
 }
